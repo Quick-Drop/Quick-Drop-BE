afca745a71434a7ef5cb94ec77683daf86fa2bbc
s00ngle <dydtns9815@naver.com>
2024-02-01 16:24:43 +0900

    Test 클래스 삭제

--- models.py
+++ models.py
@@ -3,13 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Test(Base):
-    __tablename__ = 'test'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(TEXT, nullable=False)
-    number = Column(Integer, nullable=False)
-
 class User(Base):
     __tablename__ = 'user'
 
